fix depth computation

ldm/models/diffusion/ddpm_diffmap.py
@@ -158,7 +158,7 @@ class DDPMDiffmap(DDPM):
         out = {m: split_all[m] for m in modalities}
         return out
 
-    def _get_denoise_row_from_list(self, samples, desc='', force_no_decoder_quantization=False):
+    def _get_denoise_row_from_list(self, samples, desc='', modality=None):
         denoise_row = []
         for zd in tqdm(samples, desc=desc):
             denoise_row.append(zd.to(self.device))
@@ -166,6 +166,8 @@ class DDPMDiffmap(DDPM):
         denoise_row = torch.stack(denoise_row)  # n_log_step, n_row, C, H, W
         denoise_grid = rearrange(denoise_row, 'n b c h w -> b n c h w')
         denoise_grid = rearrange(denoise_grid, 'b n c h w -> (b n) c h w')
+        if modality in ['depth_ctxt', 'depth_trgt']:
+            denoise_grid = self.to_depth(denoise_grid)
         denoise_grid = make_grid(denoise_grid, nrow=n_imgs_per_row)
         return denoise_grid
 
@@ -267,17 +269,13 @@ class DDPMDiffmap(DDPM):
         ) -> tuple[dict[str, Tensor], dict[str, Tensor], Float[Tensor, "batch frame height width"]]:
         # Prepare depth, should be (batch frame height width).
         correspondence_weights = correspondence_weights[:, None, :, :]
-        depths_recon = torch.stack([
-            x_recon_flowmap["depth_ctxt"].mean(1),
-            x_recon_flowmap["depth_trgt"].mean(1)
+        # Compute depth with exponential mapping.
+        depths_recon = torch.cat([
+            self.to_depth(x_recon_flowmap["depth_ctxt"]),
+            self.to_depth(x_recon_flowmap["depth_trgt"])
             ],
             dim=1
         )
-        # Normalize the depth with min - max.
-        near = depths_recon.min()
-        far = depths_recon.max()
-        depths_recon = (depths_recon - near) / (far - near)
-        depths_recon = depths_recon.clip(min=0, max=1)
 
         # Prepare flow
         # flows_recon = rearrange(x_recon_flowmap["optical_flow"][:, None, :2, :, :], 'b f xy w h -> b f w h xy') #estimated clean forward flow, TODO, should be (batch pair height width 2)
@@ -689,6 +687,10 @@ class DDPMDiffmap(DDPM):
         x = nn.functional.conv2d(x, weight=self.colorize)
         x = 2. * (x - x.min()) / (x.max() - x.min()) - 1.
         return x
+
+    def to_depth(self, x: Float[Tensor, "batch 3 height width"]) -> Float[Tensor, "batch 1 height width"]:
+        depths = x.mean(1, keepdims=True)
+        return (depths / 1000).exp() + 0.01
     
     @torch.no_grad()
     def log_images(self, batch, N=8, n_row=4, sample=True, ddim_steps=200, ddim_eta=1., return_keys=None,
@@ -793,8 +795,10 @@ class DDPMDiffmap(DDPM):
             
             if sample: #sampling of the conditional diffusion model with DDIM for accelerated inference without logging intermediates
                 x_samples = samples[:, k*3:(k+1)*3,...]
-                if modality != "optical_flow":
-                    log[modality]["correspondence_weights"] = repeat(batch["correspondence_weights"], "b h w -> b c h w", c=3)
+                if modality in ['depth_trgt', 'depth_ctxt']:
+                    x_samples = self.to_depth(x_samples)
+                    if modality == 'depth_trgt': # TODO properly load correspondence weights
+                        log[modality]["correspondence_weights"] = batch["correspondence_weights"]
 
                 log[modality]["samples"] = x_samples
                 if plot_denoise_rows:
@@ -803,6 +807,8 @@ class DDPMDiffmap(DDPM):
 
             if unconditional_guidance_scale > 1.0 and self.model.conditioning_key not in ["concat", "hybrid"]: #sampling with classifier free guidance
                 x_samples_cfg = samples_cfg[: , k*3:(k+1)*3, ...]
+                if modality in ['depth_trgt', 'depth_ctxt']:
+                    x_samples_cfg = self.to_depth(x_samples_cfg)
                 log[modality][f"samples_cfg_scale_{unconditional_guidance_scale:.2f}"] = x_samples_cfg
 
             if inpaint:

main.py
@@ -20,6 +20,8 @@ from pytorch_lightning.utilities import rank_zero_info
 from ldm.data.base import Txt2ImgIterableBaseDataset
 from ldm.util import instantiate_from_config
 from flow_vis_torch import flow_to_color
+from ldm.modules.flowmap.visualization.depth import color_map_depth
+from ldm.modules.flowmap.visualization.color import apply_color_map_to_image
 
 
 
@@ -480,16 +482,22 @@ class ImageLoggerDiffmap(ImageLogger):
                     images_m[k] = images_m[k][:N].clone()
                     if isinstance(images_m[k], torch.Tensor):
                         images_m[k] = images_m[k].detach().cpu()
-                        if self.clamp:
+                        if self.clamp and modality not in ["depth_trgt", "depth_ctxt"]:
                             images_m[k] = torch.clamp(images_m[k], -1., 1.)
                     if modality == "optical_flow":
-                        images_m[k] = (flow_to_color(images_m[k][:,:2,:,:]) / 255)
+                        if k in ['inputs', 'samples']:
+                            images_m[k] = (flow_to_color(images_m[k][:,:2,:,:]) / 255)
                     elif modality in ["depth_trgt", "depth_ctxt"]:
-                        depth_map = images_m[k].mean(1) #averages prediction across three channels
-                        depth_map = (depth_map + 1)/2 #brings back in [0,1]
-                        depth_rgb = torch.stack([depth_map]*3, dim=1)
-                        images_m[k] = depth_rgb
-
+                        # TODO do it properly, remove correspondence weights from here
+                        if k == 'samples':
+                            images_m[k] = color_map_depth(images_m[k].squeeze(1))
+                        elif k == 'inputs':
+                            # depth_map = images_m[k].mean(1) #averages prediction across three channels
+                            # depth_map = (depth_map + 1)/2 #brings back in [0,1]
+                            # images_m[k] = color_map_depth(depth_map)
+                            images_m[k] = (images_m[k] + 1) /2
+                        elif k == 'correspondence_weights':
+                            images_m[k] = apply_color_map_to_image(images_m[k], "gray")
 
                 self.log_local(pl_module.logger.save_dir, split_m, images_m,
                             pl_module.global_step, pl_module.current_epoch, batch_idx)